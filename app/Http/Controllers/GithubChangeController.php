dd12500db5184dae8d6c99bac9d94fd7415226b4
raylu <90059+raylu@users.noreply.github.com>
2025-05-17 13:37:27 -0700

    break pint formatting

--- app/Http/Controllers/GithubChangeController.php
+++ app/Http/Controllers/GithubChangeController.php
@@ -1,16 +1,13 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class GithubChangeController extends Controller
-{
-    public function show(Request $request)
-    {
-        return view('githubChanges')->with([
-            'bladeGlobals' => $this->globalDataService->getBladeGlobals(),
-            'access_token' => ENV('GITHUB_PERSONAL_ACCESS_TOKEN'),
-        ]);
-    }
+class GithubChangeController extends Controller {
+	public function show(Request $request) {
+		return view('githubChanges')->with([
+			'bladeGlobals' => $this->globalDataService->getBladeGlobals(),
+			'access_token' => ENV('GITHUB_PERSONAL_ACCESS_TOKEN'),
+		]);
+	}
 }
